2d0dc65ead7363d451125adc2665ac8f05153c4e
남동호 <namdongho@namdonghoui-MacBookPro.local>
2020-11-06 19:11:20 +0900

    Creating Views

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,13 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const video_detail = (req, res) => res.render("videoDetail");
-export const edit_video = (req, res) => res.render("editVideo");
-export const delete_video = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", {pageTitle : "Home"});
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    //const searchingBy = req.query.term;
+    res.render("search", {pageTitle : "Search", searchingBy});
+};
+export const videos = (req, res) => res.render("videos", {pageTitle : "Videos"});
+export const upload = (req, res) => res.render("upload", {pageTitle : "Upload"});
+export const video_detail = (req, res) => res.render("videoDetail", {pageTitle : "Video Detail"});
+export const edit_video = (req, res) => res.render("editVideo", {pageTitle : "Edit Video"});
+export const delete_video = (req, res) => res.render("deleteVideo", {pageTitle : "Delete Video"});
