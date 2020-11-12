c85646bec38cea70ed8e6315e0ed234c7fa4ea7b
남동호 <namdongho@namdonghoui-MacBookPro.local>
2020-11-12 18:19:15 +0900

    backend work

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
-export const home = (req, res) => res.render("home", {pageTitle : "Home"});
+import { videos } from "../db";
+export const home = (req, res) => res.render("home", {pageTitle : "Home", videos});
 export const search = (req, res) => {
     const {
         query: { term: searchingBy }
@@ -6,7 +7,6 @@ export const search = (req, res) => {
     //const searchingBy = req.query.term;
     res.render("search", {pageTitle : "Search", searchingBy});
 };
-export const videos = (req, res) => res.render("videos", {pageTitle : "Videos"});
 export const upload = (req, res) => res.render("upload", {pageTitle : "Upload"});
 export const video_detail = (req, res) => res.render("videoDetail", {pageTitle : "Video Detail"});
 export const edit_video = (req, res) => res.render("editVideo", {pageTitle : "Edit Video"});
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324223,
+        title: 'Video awesome',
+        description:'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id :123123,
+            name: "Dongho",
+            email:"nico@las.com"
+        }
+    },
+    {
+        id:973838,
+        title: 'Video cool',
+        description:'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id :123123,
+            name: "Dongho",
+            email:"nico@las.com"
+        }
+    },
+    {
+        id:65434,
+        title: 'Video super',
+        description:'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id :123123,
+            name: "Dongho",
+            email:"nico@las.com"
+        }
+    },
+    {
+        id:123124,
+        title: 'Video nice',
+        description:'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id :123123,
+            name: "Dongho",
+            email:"nico@las.com"
+        }
+    }
+];
