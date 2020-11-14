d101fc28ab96b9dae5b02aea8ff2d2c7f2750019
남동호 <namdongho@namdonghoui-MacBookPro.local>
2020-11-15 03:21:17 +0900

    backend work

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,22 @@
 import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) => res.render("home", {pageTitle : "Home", videos});
 export const search = (req, res) => {
     const {
         query: { term: searchingBy }
     } = req;
     //const searchingBy = req.query.term;
-    res.render("search", {pageTitle : "Search", searchingBy});
+    res.render("search", {pageTitle : "Search", searchingBy, videos});
 };
-export const upload = (req, res) => res.render("upload", {pageTitle : "Upload"});
+export const getUpload = (req, res) => res.render("upload", {pageTitle : "Upload"});
+export const postUpload = (req, res) => {
+    const {
+        body:{ file, title, description }
+    } = req;
+
+    res.redirect(routes.videoDetail(324223));
+};
 export const video_detail = (req, res) => res.render("videoDetail", {pageTitle : "Video Detail"});
 export const edit_video = (req, res) => res.render("editVideo", {pageTitle : "Edit Video"});
 export const delete_video = (req, res) => res.render("deleteVideo", {pageTitle : "Delete Video"});
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { delete_video, edit_video, upload, videos, video_detail } from "../controllers/videoController";
+import { delete_video, edit_video, getUpload, postUpload, video_detail } from "../controllers/videoController";
 import routes from "../routes"
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, video_detail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.videoDetail(), video_detail);
 videoRouter.get(routes.editVideo, edit_video);
 videoRouter.get(routes.deleteVideo, delete_video);
 
