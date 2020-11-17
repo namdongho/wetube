fabca6f0ca1f0b63fb29db6ecd034ba1bb4ed366
남동호 <namdongho@namdonghoui-MacBookPro.local>
2020-11-18 00:15:31 +0900

    Made model, schema

--- db.js
+++ db.js
@@ -1,50 +1,19 @@
-export const videos = [
-    {
-        id:324223,
-        title: 'Video awesome',
-        description:'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id :123123,
-            name: "Dongho",
-            email:"nico@las.com"
-        }
-    },
-    {
-        id:973838,
-        title: 'Video cool',
-        description:'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id :123123,
-            name: "Dongho",
-            email:"nico@las.com"
-        }
-    },
-    {
-        id:65434,
-        title: 'Video super',
-        description:'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id :123123,
-            name: "Dongho",
-            email:"nico@las.com"
-        }
-    },
-    {
-        id:123124,
-        title: 'Video nice',
-        description:'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id :123123,
-            name: "Dongho",
-            email:"nico@las.com"
-        }
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    {   
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`🚫 Error on DB Connecton:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,6 +1,11 @@
-import app from "./app.js";
+import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => 
     console.log(`😈Listening on: http://localhost:${PORT}`);
